29434a8a09b51669cf5f9604f330453cf9993ed3
admin <admin@CHETUIWK1860>
2022-07-27 13:05:21 +0530

    Add project files.

---
+++ method_overloading_/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace method_overloading_
+{
+    class Demo
+    {
+        public void Show(int x, int y)
+        {
+            Console.WriteLine(x+y);
+        }
+
+        public void Show(string x, string y)
+        {
+            Console.WriteLine(x+y);
+        }
+
+        public void Show(float x, float y)
+        {
+            Console.WriteLine(x+y);
+        }
+
+        public void Show(double x, double y)
+        {
+            Console.WriteLine(x+y);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            d.Show(1,2);
+            d.Show("sonu kumar", "neha thakur");
+            d.Show(1.5F, 2.5F);
+            d.Show(15.5D, 12.5D);
+            Console.ReadLine();
+        }
+    }
+}
